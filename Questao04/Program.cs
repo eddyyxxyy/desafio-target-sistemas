8b9d73c14bce478a59704c68cb7228024c938fce
Edson Pimenta <dev.edsonpimenta@gmail.com>
2025-05-12 11:56:02 -0300

    feat: solves state billing participation percentage with basic math

---
+++ Questao04/Program.cs
@@ -0,0 +1,21 @@
+﻿var stateBilling = new Dictionary<string, decimal>
+    {
+        { "SP", 67836.43m },
+        { "RJ", 36678.66m },
+        { "MG", 29229.88m },
+        { "ES", 27165.48m },
+        { "Outros", 19849.53m }
+    };
+
+decimal totalBilling = 0;
+foreach (var billing in stateBilling.Values)
+{
+    totalBilling += billing;
+}
+
+Console.WriteLine("Percentual de representação dos estados no faturamento total:");
+foreach (var state in stateBilling)
+{
+    decimal participation = (state.Value / totalBilling) * 100;
+    Console.WriteLine($"{state.Key}: {participation:F2}%");
+}
